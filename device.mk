740e0aec73836c86f6594740844503a540c1240b
Pranav Vashi <neobuddy89@gmail.com>
2022-07-02 02:50:39 +0530

    nemo: overlay: Fix lockscreen charging info
    * Custom feature.
    * Requires: https://github.com/crdroidandroid/android_frameworks_base/commit/c0b53901e9a9c5def4b1947df84d13073284e3b0
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -157,7 +157,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     SettingsOverlayNemo \
