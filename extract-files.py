254dbf2477e891d31b25c266dd2a4c0c56a52e36
Sarthak Roy <sarthakroy2002@gmail.com>
2025-07-18 10:55:36 +0200

    nemo: Update Video Telephony system blobs from rothko OS3.0.4.0.WNNEUXM
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I0b3dd9dc015d5eaa36847890fe6adc3be17374ab
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -56,8 +56,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/my_product', '/product'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
-    'system_ext/lib64/libsource.so': blob_fixup()
-        .add_needed('libui_shim.so'),
     ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib/libsysenv.so', 'vendor/lib64/libsysenv.so', 'vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron') : blob_fixup()
          .add_needed('libbase_shim.so'),
     'vendor/bin/hw/mtkfusionrild': blob_fixup()
