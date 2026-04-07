17aabb2732d9e398c576c9ea9426787fc646bf7a
Mashopy <eliasgheeraert@gmail.com>
2026-04-07 10:57:08 +0200

    nemo: Rename memtrack service to the new impl
    Change-Id: I8dc85520aa16b0e5b687fa71f3b23d9b5a9807c8
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -139,7 +139,7 @@ PRODUCT_COPY_FILES += \
 
 # Memtrack
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 # NFC
 PRODUCT_PACKAGES += \
